523ac811da428370a8831d8c756d08bcd43c0b43
Artur Kandaurau <kondaurov.archi@gmail.com>
2023-12-22 15:50:07 +0300

    feat: add new working test conf

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,33 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';import { vitest } from 'vitest';
+// @vitest-environment jsdom
 
-import App from './App';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
   it('Renders hello world!', () => {
     // ARRANGE
-    render(<App />);
+    render(<WrappedApp />);
     // ACT
     // EXPECT
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
+    ).toHaveTextContent('Hello World');
+  });
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
     );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,6 @@
-import matchers from '@testing-library/jest-dom/matchers';
+import '@testing-library/jest-dom';
+
+/* import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
-expect.extend(matchers);
+expect.extend(matchers); */
