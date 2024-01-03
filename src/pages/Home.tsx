67ccfaf44ee27b35bd24c005b12d5ab606bfdc8d
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-01-03 03:08:48 +0300

    feat: add first component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
-function Home() {
+import { useState, useEffect, useRef } from "react";
+
+/* function Home() {
   return <h1>Hello World</h1>;
-}
+} */
+
+const Home: React.FC = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <div>
+      <div>
+        <input value={value} onChange={(e) => setValue(e.target.value)} />
+        <button>Search</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
