e62b420d50e833e9cdea03fd716b935cc9bfd9c1
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-02-14 00:41:43 +0300

    feat: implement console logging of api data

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
 export const SearchBar: FC = () => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value: string) => {
+    fetch("https://swapi.dev/api/vehicles")
+      .then((resp) => resp.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  const handleChange = (value: string) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
     <div className="input-wrapper">
       <FaSearch id="search-icon" />
-      <input placeholder="Type to search..." />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   );
 };
