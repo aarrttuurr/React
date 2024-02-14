ca011aa6a24a69cc5593f939647a0ea018e5acf5
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-02-14 17:14:39 +0300

    feat: implement concrete search result console logging

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,14 +2,22 @@ import { FC, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
+interface ApiResData {
+  count: number;
+  next: 
+}
+
 export const SearchBar: FC = () => {
   const [input, setInput] = useState("");
 
   const fetchData = (value: string) => {
-    fetch("https://swapi.dev/api/vehicles")
+    fetch(`https://swapi.dev/api/people/?search=${value}`)
       .then((resp) => resp.json())
       .then((json) => {
-        console.log(json);
+        const results = json.filter((person) => {
+          return value && person;
+        });
+        console.log(results);
       });
   };
 
