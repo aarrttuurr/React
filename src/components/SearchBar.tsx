3430ea46eea793bf359ef0d5e737a0901658f7ea
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-03-01 01:58:41 +0300

    feat: add useEffect and previous results useState

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,37 +1,49 @@
-import { FC, FormEvent, useState } from "react";
+import { FC, FormEvent, useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 import { ApiResData, ResourcesType } from "../types/data";
 
 export const SearchBar: FC = () => {
-  const [input, setInput] = useState("");
+  const [found, setFound] = useState<ApiResData[]>([]);
+  const [search, setSearch] = useState("");
 
-  const fetchData = (type: ResourcesType, value: string) => {
+  const searchForItems = async (
+    type: ResourcesType,
+    value: string
+  ): Promise<ApiResData> => {
+    const result = await fetch(
+      `https://swapi.dev/api/${type}/?search=${value}`
+    );
+    return (await result.json()).results;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const query = encodeURIComponent(search);
+      const response = await searchForItems(ResourcesType.Planets, query);
+      setFound(response);
+    })();
+  }, [search]);
+
+  /* const fetchData = (type: ResourcesType, value: string) => {
     fetch(`https://swapi.dev/api/${type}/?search=${value}`)
       .then((resp) => resp.json())
       .then((json: ApiResData) => {
-        /* const results = json.results.filter((person: ApiResData) => {
-          return value && person;
-        }); */
         console.log(json);
       });
-  };
+  }; */
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
     const input = form.querySelector(".search-input") as HTMLInputElement;
-    setInput(input.value);
-    fetchData(ResourcesType.Planets, input.value);
+    setSearch(input.value);
+    input.value = "";
   };
 
   return (
-    <form className="search-form">
-      <input
-        className="search-input"
-        placeholder="Type to search..."
-        onSubmit={(e) => handleSubmit(e)}
-      />
+    <form className="search-form" onSubmit={(e) => handleSubmit(e)}>
+      <input className="search-input" placeholder="Type to search..." />
       <button>
         <FaSearch id="search-icon" />
       </button>
