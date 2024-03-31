673be7f0dcd04b24995b89244a9a67dcadc8af31
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-04-01 01:01:03 +0300

    feat: add respEntity verification functions and first perk for search result depending on result type

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,18 +1,48 @@
 import "./SearchResult.css";
-import { IFilm, ResEntity } from "../types/data";
+import { IFilm, IPeople, IPlanet, ISpecie, IStarship, IVehicle, ResEntity } from "../types/data";
 
-const SearchResult = (props: {item: ResEntity}) => {
+const SearchResult = (props: { item: ResEntity }) => {
   const { item } = props;
 
   const isFilm = (value: ResEntity): value is IFilm => {
-    return 'title' in value;
+    return "title" in value;
+  };
+
+  const isPeople = (value: ResEntity): value is IPeople => {
+    return "birth_year" in value;
+  };
+
+  const isPlanet = (value: ResEntity): value is IPlanet => {
+    return "orbital_period" in value;
+  };
+
+  const isSpecie = (value: ResEntity): value is ISpecie => {
+    return "classification" in value;
+  };
+
+  const isStarship = (value: ResEntity): value is IStarship => {
+    return "model" in value;
+  };
+
+  const isVehicle = (value: ResEntity): value is IVehicle => {
+    return "model" in value;
   };
 
   return (
     <div className="search-result">
-      <div className="title"> {isFilm(item) ? item.title : item.name} </div>
-      <ul className="properties">
-
+      <div className="result-title"> {isFilm(item) ? item.title : item.name} </div>
+      <ul className="result-perks">
+        <li className="perk">
+          {isFilm(item)
+            ? item.director
+            : isPeople(item)
+              ? item.birth_year
+              : isPlanet(item)
+                ? item.diameter
+                : isSpecie(item)
+                  ? item.classification
+                  : item.model}
+        </li>
       </ul>
     </div>
   );
