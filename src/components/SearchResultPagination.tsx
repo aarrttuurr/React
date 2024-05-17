6b0714abaadb18c0316857ecad5e0a14462bc1b5
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-05-17 15:48:46 +0300

    feat: minor update of pagination component

--- src/components/SearchResultPagination.tsx
+++ src/components/SearchResultPagination.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import { ApiData } from "../types/data";
 
 const SearchResultPagination = (props: { data: ApiData }) => {
   const { data } = props;
+  const [page, setpage] = useState("");
+
+  const pageQuery = async (url: string): Promise<ApiData> => {
+    const result = await fetch(url);
+    return await result.json();
+  };
+
+  const handleNextPageClick = () => {
+    data.next && 
+  }
+
+  const handlePrevPageClick = () => {
+
+  }
+
   return (
-    
+    <div className="search-pagination">
+      <button className="search-prev-btn" onClick={handlePrevPageClick}>{"<"}</button>
+      <button className="search-next-btn" onClick={handleNextPageClick}>{">"}</button>
+    </div>
   );
 };
 
