9796db002c42f39a1e8890aabfc74f366eaae8b1
Artur Kandaurau <kondaurov.archi@gmail.com>
2024-05-30 17:25:46 +0300

    feat: add more raw updates to SearchSwitch component

--- src/components/SearchSwitch.tsx
+++ src/components/SearchSwitch.tsx
@@ -1,19 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent } from "react";
 import { ResourcesType } from "../types/data";
 
 interface SwitchProps {
+  searchEntity: ResourcesType;
   setSearchEntity: React.Dispatch<React.SetStateAction<ResourcesType>>;
 }
 
-const SearchSwitch: FC<SwitchProps> = ({ setSearchEntity }) => {
+const SearchSwitch: FC<SwitchProps> = ({ searchEntity, setSearchEntity }) => {
+  const changeSelection = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchEntity(e.target.value);
+  };
+
+  function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+    return Object.keys(obj).filter((k) => !Number.isNaN(k)) as K[];
+  }
+
   return (
     <fieldset>
       <legend>What are you looking for?</legend>
       <div>
-        {}
+        {enumKeys(ResourcesType).map((_key, i) => {
+          return (
+            <>
+              <input
+                type="radio"
+                id="entity1"
+                name="entity"
+                key={i + 1}
+                value={ResourcesType[_key]}
+                onChange={(e) => changeSelection(e)}
+                checked={searchEntity === ResourcesType[_key]}
+              />
+              <label htmlFor="contactChoice1">{ResourcesType[_key]}</label>
+            </>
+          );
+        })}
       </div>
     </fieldset>
   );
-}
+};
 
 export default SearchSwitch;
